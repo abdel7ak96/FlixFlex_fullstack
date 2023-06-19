cf25acecfc0601fb8b1740f8be026e42e490fabb
Abdelhak RAHMOUNI <rahmouni.abdelhak.1@gmail.com>
2023-06-19 22:09:30 +0100

    Implement: complete auth UI

--- components/Header/auth.tsx
+++ components/Header/auth.tsx
@@ -1,9 +1,22 @@
-const Auth = ({ className }: { className?: string }) => {
-  const connected = true;
+import { useState } from 'react';
 
-  return (
+const Auth = ({ className }: { className?: string }) => {
+  const [connected, setConnected] = useState<boolean>(false);
+
+  const disconnectedComp = (
+    <div className={`${className}`}>
+      <button
+        onClick={() => setConnected(true)}
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
+      >
+        Login
+      </button>
+    </div>
+  );
+
+  const connnectedComp = (
     <div className={`${className} m-4 flex justify-end items-center space-x-2`}>
-      <button className="hover:cursor-pointer">
+      <button className="hover:cursor-pointer" title='Favorites list'>
         <div>
           <svg
             className="h-8"
@@ -21,15 +34,32 @@ const Auth = ({ className }: { className?: string }) => {
             />
           </svg>
         </div>
-        {/* <div className="relative right-4 bottom-4 bg-red-500 p-1 px-2 rounded-full text-white text-xs">
+        
+        {/* 
+        /// TODO: Chip displaying count of favorite movies, Add if you had time
+        <div className="relative right-4 bottom-4 bg-red-500 p-1 px-2 rounded-full text-white text-xs">
           1
         </div> */}
       </button>
-      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
-        Login
-      </button>
+      <div>
+        <button
+          onClick={() => setConnected(false)}
+          className="bg-slate-300 hover:bg-slate-400 flex items-center w-32 rounded-full p-1"
+        >
+          <div className="relative content-start inline-flex items-center justify-center w-8 h-8 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
+            <span className="font-medium text-gray-600 dark:text-gray-300">
+              JL
+            </span>
+          </div>
+          <span className="content-center text-white font-bold px-2">
+            Sign out
+          </span>
+        </button>
+      </div>
     </div>
   );
+
+  return connected ? connnectedComp : disconnectedComp;
 };
 
 export default Auth;
